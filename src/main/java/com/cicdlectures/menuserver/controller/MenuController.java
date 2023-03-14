0042a05a45c84871b8207925a04c14ca37716faf
Baran Ilona <ilona.baran@ensg.eu>
2023-03-14 15:44:53 +0000

    bon code

--- src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
+++ src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import com.cicdlectures.menuserver.dto.MenuDto;
 import com.cicdlectures.menuserver.service.CreateMenuService;
 import com.cicdlectures.menuserver.service.ListMenuService;
+import com.cicdlectures.menuserver.repository.MenuRepository;
 
 @RestController
 public class MenuController {
